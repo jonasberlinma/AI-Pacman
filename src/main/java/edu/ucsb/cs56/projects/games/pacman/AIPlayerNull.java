29561d45a82fb955a7a1a8ef10a3d618993134ac
Jonas Berlin <jonas@theberlins.org>
2017-03-11 00:23:22 -0500

    Added dummy player to consume messages

---
+++ src/main/java/edu/ucsb/cs56/projects/games/pacman/AIPlayerNull.java
@@ -0,0 +1,11 @@
+package edu.ucsb.cs56.projects.games.pacman;
+
+public class AIPlayerNull extends AIPlayer {
+
+	@Override
+	protected void dataEvent(DataEvent dataEvent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
