9978579a8b275010eaac7ee9422ac78b6d545b94
Jonas Berlin <jonas@theberlins.org>
2017-03-14 11:14:03 -0400

    Added method for getting key value pairs

--- src/main/java/edu/ucsb/cs56/projects/games/pacman/EventTrackable.java
+++ src/main/java/edu/ucsb/cs56/projects/games/pacman/EventTrackable.java
@@ -1,7 +1,10 @@
 package edu.ucsb.cs56.projects.games.pacman;
 
+import java.util.Map;
+
 public interface EventTrackable {
 
 	public long getGameID();
 	public int getGameStep();
+	public Map<String, String> getData();
 }
