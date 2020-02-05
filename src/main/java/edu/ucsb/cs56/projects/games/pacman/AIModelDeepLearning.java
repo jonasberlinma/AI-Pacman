81b339043b09b7a52f260b57851246f09292f68b
Jonas Berlin <jonas@theberlins.org>
2020-02-05 15:07:12 -0500

    Forgot to add Direction

--- src/main/java/edu/ucsb/cs56/projects/games/pacman/AIModelDeepLearning.java
+++ src/main/java/edu/ucsb/cs56/projects/games/pacman/AIModelDeepLearning.java
@@ -15,7 +15,6 @@ import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.nd4j.linalg.activations.Activation;
-import org.nd4j.linalg.api.buffer.DataBuffer;
 import org.nd4j.linalg.api.buffer.DataType;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
---
+++ src/main/java/edu/ucsb/cs56/projects/games/pacman/Direction.java
@@ -0,0 +1,65 @@
+package edu.ucsb.cs56.projects.games.pacman;
+
+public enum Direction {
+	LEFT, RIGHT, UP, DOWN;
+	public boolean isSame(Direction other) {
+		return this == other;
+	}
+
+	public boolean isOpposite(Direction other) {
+		return this == other.opposite();
+	}
+
+	public Direction opposite() {
+		Direction ret = null;
+		switch (this) {
+		case LEFT:
+			ret = RIGHT;
+			break;
+		case RIGHT:
+			ret = LEFT;
+			break;
+		case UP:
+			ret = DOWN;
+			break;
+		case DOWN:
+			ret = UP;
+			break;
+		}
+		return ret;
+	}
+
+	static Direction parseDirection(String text) {
+		Direction d = null;
+		if (text != null) {
+			switch (text) {
+			case "LEFT":
+			case "←":
+			case "1":
+				d = Direction.LEFT;
+				break;
+			case "RIGHT":
+			case "→":
+			case "2":
+				d = Direction.RIGHT;
+				break;
+			case "DOWN":
+			case "↓":
+			case "3":
+				d = Direction.DOWN;
+				break;
+			case "UP":
+			case "↑":
+			case "4":
+				d = Direction.UP;
+				break;
+			case "0":
+				break;
+			default:
+				System.err.println("Failed to parse direction " + text);
+			}
+		}
+		return d;
+	}
+};
+
